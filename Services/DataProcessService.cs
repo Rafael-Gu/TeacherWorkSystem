5b61df10a58825361e525c33a813710bf7ca9ab1
Oberon <360119124@qq.com>
2020-02-13 23:15:34 +0800

    Commit

---
+++ Services/DataProcessService.cs
@@ -0,0 +1,48 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using TeacherWork.Models;
+
+
+namespace TeacherWork.Services
+{
+	public class DataProcessService : IDataProcessService
+	{
+
+		private List<Teacher> teachers;
+		private List<Course> courses;
+		private List<Subject> subjects;
+		private List<Task> tasks;
+
+		private IWorkbook workbook = null;
+
+		public List<ICourseService> GetCourseStat()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ImportFromExcel(string filename)
+		{
+			try
+			{
+				workbook = new HSSFWorkbook(new FileStream(filename, FileMode.Open));
+				ISheet worksheet = workbook.GetSheet("主修课程");
+
+
+			}
+			catch (FileNotFoundException)
+			{
+				throw;
+			}
+		}
+
+		public void Process()
+		{
+			if (workbook == null)
+				return;
+
+		}
+	}
+}
---
+++ Services/IDataProcessService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace TeacherWork.Services
+{
+	public interface IDataProcessService
+	{
+		public void ImportFromExcel(string filename);
+		public void Process();
+		public List<ICourseService> GetCourseStat();
+
+	}
+}
